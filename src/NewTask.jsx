7cc7e5616e4b0cad1544f5ea9f33100ca63ab305
NotN465 <eceljak@gmail.com>
2026-03-28 13:41:49 +0100

    Modified the CSS of the buttons (just a little bit ), also made the new task button work. You can create new tasks now.

--- src/NewTask.jsx
+++ src/NewTask.jsx
@@ -7,6 +7,10 @@ export const NewCategory = () => {
   // This arrow function parses the categories string from the local storage as a hashmap
   // and then adds the new category (the value is set to true for all available categories)
   const onSubmit = () => {
+      if (categoryName == ""){
+        alert("You must enter a category name.");
+        return;
+      }
       const categories = JSON.parse(localStorage.getItem("categories")) || {};
       categories[categoryName] = true 
       localStorage.setItem("categories",JSON.stringify(categories));
@@ -36,10 +40,53 @@ export const NewCategory = () => {
 
 export const NewTask = () => {
   const [taskWindowOpen, setTaskWindowOpen] = useState(false);
-  const createNewTask = () => {
-  };
-  
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [taskName, setTaskName] = useState("");
+  const [description, setDescription] = useState("");
 
+  let categories;
+
+  const getCategories = () => {
+    const data = localStorage.getItem("categories");
+    categories = JSON.parse(data);
+    // console.log(categories, Object.keys(categories));
+     
+    return Object.keys(categories);
+  }
+
+  const logTasks =() => {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.map((task) => {
+      console.log(task)
+    })
+  }
+  
+  const closeTaskWindow = () => {
+    setTaskName("");
+    setDescription("");
+    setSelectedCategory("");
+    setTaskWindowOpen(false);
+    logTasks();
+  } 
+
+  const createNewTask = () => {
+     if (selectedCategory === "" || taskName == ""){
+      alert("All fields must be filled (except the description field)"); 
+      return;
+    } 
+    closeTaskWindow();
+    const newTaskEntry = {
+      id: crypto.randomUUID(),
+      taskName: taskName,
+      description: description,
+      category: selectedCategory,
+      completed: false
+    };
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.push(newTaskEntry);
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+
+  };
 
   return (
   <div>
@@ -49,14 +96,21 @@ export const NewTask = () => {
           <div className="modalStyle">
             <h2>New Task</h2>
             <h3>Task Name</h3>
-            <input placeholder="Enter task name" />
+            <input placeholder="Enter task name" 
+            value={taskName} onChange={ (e) => { setTaskName(e.target.value); } }></input>
             <br />
             <h3>Description</h3>
-            <input placeholder="Enter description (optional)"></input>
+            <input placeholder="Enter description (optional)"
+            value={description} onChange={(e) => { setDescription(e.target.value); } }></input>
             <h3>Task category</h3>
-            {  }
+            { getCategories().map((category, index) => {
+             return (<button key={index}
+              onClick={() => { setSelectedCategory(category) } }
+              className={ selectedCategory === category ? "selectedButton" : "nonSelectedButton" }>{category}</button>)
+            })  }
             <br/>
-            <button onClick={() => setTaskWindowOpen(false)}> X Close</button>
+            <button onClick={createNewTask}>Submit</button>
+            <button onClick={closeTaskWindow}> X Close</button>
           </div>
         </div>
       ) : null }  
